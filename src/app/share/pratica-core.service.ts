4762b3f97d1595c7524e49a15ea97a2facd4eed7
Edney Chantal <praticasolucoesti@gmail.com>
2017-01-12 13:22:26 -0300

    implementacao mascara

--- src/app/share/pratica-core.service.ts
+++ src/app/share/pratica-core.service.ts
@@ -11,14 +11,48 @@ export class PraticaCore {
         return parseInt( str.replace(/[\D]+/g,'') );
         }
 
-   textToMoeda( int:number):string 
-    {
-        var tmp = int+'';
-        tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
-        if( tmp.length > 6 )
-                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+   textToMoeda( data:string):string{
+        let sinal = 1; 
+        sinal = 1;    
+        let retorno:string;
+        if (data.substr(0,1) === '-') {
+           sinal = -1;    
+        }
+        if (data.substr(0,1) === '(') {
+           sinal = -1;    
+        }
+		data  = data.replace(/[^0-9]+/g,"");
+        data  = parseInt(data).toString();
+        if (parseInt(data) === 0){
+             return '0';
+        } else if ((data === "") && (sinal < 0)) {
+           return "-";
+        }
+        if (data.length == 1 ) {
+           retorno = '0,0' + data;
+        }
+        if (data.length == 2 ) {
+           retorno = '0,' + data;
+        }
+        console.log(data);
+        if (data.length >= 3) {
+           let partedecimal:string = data.substr(data.length-2,2);
+           let parteinteira:string  = data.substr(0,data.length-2);
+           let qtdend :number = parteinteira.length%3; 
+           let partemeio:string = parteinteira.substr(qtdend);
+           let qtdpontos :number= Math.trunc(parteinteira.length/3);
 
-        return tmp;
+           // inicio
+           retorno = parteinteira.substr(0,qtdend);
+           // meio
+           for (let i = 0;i<=(qtdpontos-);i++) {
+                retorno += '.'
+                retorno += partemeio.substr(i*3,3);
+           }
+           // decimal
+           retorno += "," + partedecimal;
+        }
+        return retorno;
     }
     
  
